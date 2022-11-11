33a436adb54f032d53a2c17e6a9bfb1e5d59e4d6
Enzo M <enzojmayo@gmail.com>
2022-11-11 04:31:32 -0600

    Fix manifest icon URL not resolving correctly

--- vite.config.js
+++ vite.config.js
@@ -16,43 +16,43 @@ export default defineConfig({
                 theme_color: '#90ee90',
                 icons: [
                     {
-                        src: '/images/maskable_icon_x48.png',
+                        src: 'images/maskable_icon_x48.png',
                         type: 'image/png',
                         sizes: '48x48',
                         purpose: 'any maskable'
                     },
                     {
-                        src: '/images/maskable_icon_x72.png',
+                        src: 'images/maskable_icon_x72.png',
                         type: 'image/png',
                         sizes: '72x72',
                         purpose: 'any maskable'
                     },
                     {
-                        src: '/images/maskable_icon_x96.png',
+                        src: 'images/maskable_icon_x96.png',
                         type: 'image/png',
                         sizes: '96x96',
                         purpose: 'any maskable'
                     },
                     {
-                        src: '/images/maskable_icon_x128.png',
+                        src: 'images/maskable_icon_x128.png',
                         type: 'image/png',
                         sizes: '128x128',
                         purpose: 'any maskable'
                     },
                     {
-                        src: '/images/maskable_icon_x192.png',
+                        src: 'images/maskable_icon_x192.png',
                         type: 'image/png',
                         sizes: '192x192',
                         purpose: 'any maskable'
                     },
                     {
-                        src: '/images/maskable_icon_x384.png',
+                        src: 'images/maskable_icon_x384.png',
                         type: 'image/png',
                         sizes: '384x384',
                         purpose: 'any maskable'
                     },
                     {
-                        src: '/images/maskable_icon_x512.png',
+                        src: 'images/maskable_icon_x512.png',
                         type: 'image/png',
                         sizes: '512x512',
                         purpose: 'any maskable'
